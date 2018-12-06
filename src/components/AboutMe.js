ea5583dea4c4f5a4cb2c7fb064353792fd25add6
Nikolas Melgarejo <nikoclops@gmail.com>
2018-12-05 21:50:00 -0500

    Adds changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,23 +13,38 @@ export default function({ data }) {
 						style={{ display: 'flex', justifyContent: 'flex-end' }}
 					/>
 					<AboutMidText>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nam
-							dicta quidem est dolore labore sed nemo neque explicabo a autem
-							repudiandae laboriosam laudantium optio alias quas dignissimos
-							iure, unde quos eum odio non, libero tempore? Nam praesentium
-							ipsum unde blanditiis ipsa, laborum placeat beatae sunt magnam
-							cupiditate omnis aperiam.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit.
-							Laudantium optio suscipit maiores. Provident nam molestias
-							suscipit reiciendis eveniet laborum consequuntur est nesciunt
-							perspiciatis iure explicabo, eius vitae ipsam, ut ex quia eos
-							veniam accusantium! Iste odit, ut molestias magnam temporibus
-							iusto harum laboriosam nihil modi, excepturi voluptates officiis
-							veritatis dolor!
-						</p>
+						<div>
+							<AboutMidSectionH2>What I push myself to be.</AboutMidSectionH2>
+							<AboutMidSectionP>
+								Ever since I was young I always looked to become{' '}
+							</AboutMidSectionP>
+						</div>
+						<div>
+							<AboutMidSectionH2>What I am now</AboutMidSectionH2>
+							<AboutMidSectionP>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Laudantium optio suscipit maiores. Provident nam molestias
+								suscipit reiciendis eveniet laborum consequuntur est nesciunt
+								perspiciatis iure explicabo, eius vitae ipsam, ut ex quia eos
+								veniam accusantium! Iste odit, ut molestias magnam temporibus
+								iusto harum laboriosam nihil modi, excepturi voluptates officiis
+								veritatis dolor!
+							</AboutMidSectionP>
+						</div>
+						<div>
+							<AboutMidSectionH2>
+								Life outside of Development?
+							</AboutMidSectionH2>
+							<AboutMidSectionP>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Laudantium optio suscipit maiores. Provident nam molestias
+								suscipit reiciendis eveniet laborum consequuntur est nesciunt
+								perspiciatis iure explicabo, eius vitae ipsam, ut ex quia eos
+								veniam accusantium! Iste odit, ut molestias magnam temporibus
+								iusto harum laboriosam nihil modi, excepturi voluptates officiis
+								veritatis dolor!
+							</AboutMidSectionP>
+						</div>
 					</AboutMidText>
 				</AboutMidSection>
 			</AboutSection>
@@ -52,23 +67,42 @@ const H2 = styled.h2`
 
 const AboutMidSection = styled.div`
 	display: grid;
-	grid-template-columns: 1fr .2fr 1fr;
-	grid-template-rows: 1fr
+	margin: 0 auto;
+	width: 80%;
+	grid-template-columns: 1fr 0.8fr;
+	grid-template-rows: 1fr 1fr 1fr 1fr;
 	justify-content: space-between;
 	align-items: center;
+	grid-column-gap: 1em;
 `;
 
 const Img2 = styled(Img)`
 	flex: 1;
 	display: block;
 	justify-content: center;
-	grid-column: 1/2;
+	grid-column: 1/1;
 	& img {
 		position: none;
 		align-self: center;
+		padding: 1rem;
+		width: 500px;
 	}
 `;
 
 const AboutMidText = styled.div`
-	grid-column-start: 3;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	height: 100%;
+	grid-row: 1/2;
+	grid-column: 2/3;
+`;
+
+const AboutMidSectionH2 = styled.h2`
+	font-family: Lora;
+	font-size: 1.6rem;
+`;
+
+const AboutMidSectionP = styled.p`
+	font-family: Raleway;
 `;
